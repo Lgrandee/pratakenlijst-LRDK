4dc6da5a59c197c73c919365ce5720af8a1da26f
RemousArkarou <dds0022@edu.rocwb.nl>
2024-04-03 09:16:02 +0200

    updated taskview,php

--- taskview.php
+++ taskview.php
@@ -4,45 +4,35 @@
     <?php require_once("head.php") ?>
     <title>Document</title>
 </head>
-<body>
-    <main>
-        <?php require_once("backend/conn.php"); ?>
-        <table>
-            <tr>
-                <th>titel</th>
-                <th>beschrijving</th>
-                <th>afdeling</th>
-                <th>status</th>
-                <th>deadline</th>
-                <th>user</th>
-                <th>created_at</th>
-            </tr>
+    <body class="nav-open">
+        <header>
+            <?php require_once("header.php"); ?>
+            <div class="Logo_box">
 
-            <?php 
-                require_once("backend/config.php");
-                $query2 = "SELECT * FROM taken WHERE NOT status = 'done'";
-                $statement = $conn->prepare($query2);
-                $statement->execute();
-                $taken2 = $statement->fetchAll(PDO::FETCH_ASSOC);
+            </div>
+        </header>
+        <main>
+            <?php require_once("backend/conn.php"); ?>
+            <div class="wrapper">
+                <div class="bigH">
+                    <h1>Op deze pagina zie je <span class="S1">alle taken</span> die u nog moet doen!</h1>
+                </div>
+                <div class="tablele">
+                            <?php require_once("card.php"); ?>
 
-            ?>
-
-
-            <?php foreach($taken2 as $taak2): ?>
-
-                <tr>
-                    <td> <?php echo $taak2['titel']; ?> </td>
-                    <td> <?php echo $taak2['beschrijving']; ?> </td>
-                    <td> <?php echo $taak2['afdeling']; ?> </td>
-                    <td> <?php echo $taak2['status']; ?> </td>
-                    <td> <?php echo $taak2['deadline']; ?> </td>
-                    <td> <?php echo $taak2['user']; ?> </td>
-                    <td> <?php echo $taak2['created_at']; ?> </td>
-
-                </tr>
-
-            <?php endforeach;?>
-        </table>
-    </main>
-</body>
+                </div>
+            </div>
+        </main>
+        <div class="bigfoot">
+            <!-- footer -->
+            <footer class="Footer">
+                <?php
+                require_once('footer.php');
+                ?>
+            </footer>
+            <!-- footer -->
+            <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+            <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        </div>
+    </body>
 </html>
