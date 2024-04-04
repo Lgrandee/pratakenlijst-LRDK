52ba420b769e581c96bfef64d3e7829cb6867495
Armanivr <armanivrijswijk@gmail.com>
2024-04-04 11:28:21 +0200

    fixed merge conflict issue on header.php

---
+++ backend/app/Http/Controllers/registreren.php
@@ -0,0 +1,15 @@
+<?php
+$naam =$_POST['naam'];
+$achternaam = $_POST['achternaam'];
+$email =$_POST['email'];
+$username= $_POST['username'];
+$password= $_POST['password'];
+
+require_once '../../../config.example.php';
+
+$query = "INSERT INTO users (naam, achternaam, email, username, `password`)
+VALUES (:naam, :achternaam, :email, :username, :password)";
+
+$statement = $conn->prepare($query);
+
+?>
