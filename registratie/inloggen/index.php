46d2d6fb8abc3a35ad0c3ed4cc3f433579b29732
Armanivr <armanivrijswijk@gmail.com>
2024-04-16 13:22:15 +0200

    index.php van reg en login

--- registratie/inloggen/index.php
+++ registratie/inloggen/index.php
@@ -1,80 +1,26 @@
 <!DOCTYPE html>
-<html lang="nl">
-
+<html lang="en">
 <head>
-    <title>inlogen</title>
-    <?php
-    require_once('head.php');
-    ?>
-    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
 </head>
-
-<body class="nav-open">
-    <!-- sidebar -->    
-    <?php
-    require_once('header.php');
-    ?>
-       <div class="container">
-        <h1>Meldingen</h1>
-        <a href="create.php">Nieuwe melding &gt;</a>
-
-        <?php if(isset($_GET['msg']))
-        {
-            echo "<div class='msg'>" . $_GET['msg'] . "</div>";
-        } ?>
-
-    <!-- <div style="height: 300px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">(hier komen de storingsmeldingen)</div> -->
-    <?php
-        require_once'../../backend/conn.php';
-        $query = "SELECT * FROM meldingen";
-        $statement = $conn->prepare($query);
-        $statement->execute();  
-        $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
-        ?>
-        <table>
-            <tr>
-                <th>Attractie</th>
-                <th>Type</th>
-                <th>Melder</th>
-                <th>overige info</th>
-                <th>prioriteit</th>
-                <th>aanpassen</th>
-            </tr>
-            <?php foreach($meldingen as $melding): ?>
-                <tr>
-                    <td><?php echo $melding['attractie']; ?></td>
-                    <td><?php echo $melding['type']; ?></td>
-                    <td><?php echo $melding['melder']; ?></td>
-                    <td><?php echo $melding['overige_info']; ?></td>
-                    <td>
-                        <?php if ($melding['prioriteit'] == 1) {
-                            echo "Ja";
-                        } else {
-                            echo "Nee";
-                        }
-                        ?>
-                    </td>
-                    <td><a href="edit.php?id=<?php echo $melding['id']; ?>">aanpassen</a></td>
-                </tr> 
-            
-            <?php endforeach; ?>    
-        </table>
-</div>
-
-    
-  
-
-              <!-- footer -->
-<footer class="Footer">
-<?php
-require_once('footer.php');
-?>
-</footer>
-        <!-- footer -->
-        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-
-
+<body>
+    <form action="../../backend/app/Http/Controllers/userController.php" method="POST">
+        <input type="hidden" name="action" value="login">
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <button type="submit">Login</button>
+    </form>
 </body>
-
 </html>
